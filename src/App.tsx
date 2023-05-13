bb495340509002139ae03cfef9f4c24a1941d36a
Leon Ahuja <leon.ahuja@tandm.ai>
2023-05-13 17:08:25 +1000

    FInished :)

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,24 @@ import { MainContainer, ChatContainer, MessageList, Message, MessageInput, Messa
 import { LexRuntimeV2 } from 'aws-sdk'
 // import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 const launchRatios = [
+
   "800x600",
   "1280x720",
 ]
 const test: MessageModel[] = [{ direction: "incoming", message: "Hello, how can I help you today?", position: "normal" }]
 
+const MAX_SECONDS_PERDAY = 5;
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
   // const [location, setLocation] = useState("");
   const [location, setLocation] = useState(String.raw`D:/rm_dashboard`);
   const [skipStagnate, setSkipStagnate] = useState(true);
-  const [launchRatio, setLauchRatio] = useState(0);
+  const [launchRatio, setLauchRatio] = useState(1);
   // const [messages, setMessages] = useState<MessageModel[]>([]);
   const [messages, setMessages] = useState<MessageModel[]>(test);
-  const [secondsPerDay, setSeconds] = useState<number>(0.5);
+  const [secondsPerDay, setSeconds] = useState<number>(50);
+  const [outputVideo, setOutputVideo] = useState<boolean>(false);
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -50,11 +53,15 @@ function App() {
     // const child = command.spawn();
     // console.log({ child, command });
     // `--viewport`, `${launchRatios[launchRatio]}`
-    const currentRation = launchRatios[launchRatio];
+    const currentRation = launchRatios[launchRatio - 1];
     let args = [`${location}`, `-${currentRation}`];
     if (skipStagnate) {
       args = args.concat([`--auto-skip-seconds`, `0.1`]);
     }
+
+    let minSecs = Math.max(0.1, (secondsPerDay / 100) * MAX_SECONDS_PERDAY);
+    args = args.concat([`-s`, `${minSecs}`]);
+
     console.log({ args });
 
 
@@ -95,20 +102,60 @@ function App() {
             />
             <div className="mt-6">
               <label className="block mb-2 text-sm font-medium">Seconds per Day</label>
-              <input id="default-range" type="range" className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+              <input
+                id="default-range"
+                type="range"
+                className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                value={secondsPerDay}
+                min={0}
+                max={100}
+                onChange={(e) => setSeconds(parseInt(e.target.value))}
+              />
             </div>
+            <div className="w-full flex justify-between text-xs px-2">
+              <span>|</span>
+              <span>|</span>
+              <span>|</span>
+              <span>|</span>
+              <span>|</span>
+            </div>
+            <div className="w-full flex justify-between text-xs px-2">
+              <span>0</span>
+              <span>{MAX_SECONDS_PERDAY}</span>
+            </div>
+
             <div className="mt-6">
               Viewport
-              <div className="flex items-center my-4">
+              {/* <div className="flex items-center my-4">
                 <input id="default-radio-1" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                 <label className="ml-2 text-sm font-medium">Fullscreen</label>
               </div>
               <div className="flex items-center">
                 <input id="default-radio-2" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                 <label className="ml-2 text-sm font-medium">Windowed</label>
+              </div> */}
+
+              <div className="mt-6">
+                <label className="label cursor-pointer">
+                  <span className="label-text">Skip Stagnating days</span>
+                  <input checked={skipStagnate}
+                    onChange={() => setSkipStagnate(!skipStagnate)}
+                    type="checkbox" className="checkbox" />
+                </label>
+              </div>
+
+              <div className="mt-6">
+                <label className="label cursor-pointer">
+                  <span className="label-text">Output video</span>
+                  <input type="checkbox" checked={outputVideo}
+                    onChange={() => setOutputVideo(!outputVideo)}
+                    className="checkbox" />
+                </label>
               </div>
             </div>
-            <div className="flex flex-col my-6">
+
+
+            {/* <div className="flex flex-col my-6">
               <p className="text-left mb-3">Date Range</p>
               <div className="flex flex-row">
                 <input placeholder="Start date"
@@ -118,7 +165,7 @@ function App() {
                   className="input input-bordered rounded-lg text-xs w-1/2"
                 ></input>
               </div>
-            </div>
+            </div> */}
           </div>
 
 
