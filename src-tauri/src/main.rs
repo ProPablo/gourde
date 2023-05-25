31090648c82528b6977648d8fdfc5d7cdeb96236
Leon Ahuja <leon.ahuja@tandm.ai>
2023-05-26 07:41:59 +1000

    tobe tested: conditional os spawn sidecar

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,13 +7,19 @@ use std::{
 };
 
 use anyhow::Result;
+use tauri::api::process::Command as TauriCommand;
 use tauri::{AppHandle, Config, Manager, Runtime, State};
 
 struct GourceContainer {
+    #[cfg(windows)]
     child: Mutex<Option<Child>>,
+    #[cfg(not(windows))]
+    child: Mutex<Option<TauriCommand>>,
 }
 
-fn kill_old_child(maybe_old_child: &mut Option<Child>) -> Result<bool> {
+
+
+fn kill_old_child(#[cfg(not(windows))] maybe_old_child: &mut Option<TauriCommand>, #[cfg(windows)] maybe_old_child: &mut Option<Child>) -> Result<bool> {
     match maybe_old_child.take() {
         Some(mut old_thread) => {
             println!("Stopping old child");
@@ -26,9 +32,11 @@ fn kill_old_child(maybe_old_child: &mut Option<Child>) -> Result<bool> {
 }
 
 #[tauri::command]
-fn kill_child(state: State<'_, GourceContainer>) {
+async fn kill_child(state: State<'_, GourceContainer>) -> Result<(), String> {
     let mut maybe_child = state.child.lock().unwrap();
+    // TODO: use string as error propogation
     kill_old_child(&mut maybe_child);
+    Ok(())
 }
 
 #[tauri::command]
@@ -37,46 +45,54 @@ async fn run_gource<R: Runtime>(
     args: Vec<String>,
     state: State<'_, GourceContainer>,
 ) -> Result<(), String> {
-    if std::env::consts::OS == "windows" {
-        let mut maybe_child = state.child.lock().unwrap();
+    let mut maybe_child = state.child.lock().unwrap();
+    #[cfg(windows)]
+    {
+        // Ensure directory exists and is git repo before continueing execution
         // TODO: catch error and send err event
         kill_old_child(&mut maybe_child);
 
-        // format!("Hello, {}! You've been greeted from Rust!", name)
         let mut gource_path =
             tauri::api::path::resource_dir(app.package_info(), &app.env()).unwrap();
         gource_path.push("bin/gource");
         let mut gource_bin = gource_path.clone();
         gource_bin.push("gource");
-        println!("{:?}", gource_path);
-        println!("{:?}", gource_bin);
         let final_string = gource_bin
             .to_str()
             .unwrap()
             .to_string()
+            // These characters are for long file matching which is unlikley in this case since the location of bin is likely program files, causes resource load error in gource
             .trim_start_matches("\\\\?\\")
             .to_string();
-        println!("{:?}", final_string);
+        println!("Gource path:{:?}", final_string);
 
         let output = Command::new(final_string)
             .current_dir(gource_path)
-            .args(args)
+            .args(&args)
             // .arg("D:/rm_dashboard")
             .spawn()
+            // Dont expect here since that poisons the mutex, instead return error with string explaining what happend
             .expect("failed to execute process");
         // TODO: make this into an arc that is shared across a thread that monitors when its dead, also works for checking when window is dead, and subbing to on_window_event
         // https://github.com/tauri-apps/tauri/discussions/3273
         *maybe_child = Some(output);
-    } else {
-        let res = tauri::api::process::Command::new_sidecar("gource")
-            .expect("failed to create `my-sidecar` binary command")
-            .args(args)
-            .spawn();
-        println!("{:?}", res);
-
-        // .spawn();
     }
+    #[cfg(not(windows))]
+    {
+        let res: std::result::Result<
+            (
+                tauri::async_runtime::Receiver<tauri::api::process::CommandEvent>,
+                tauri::api::process::CommandChild,
+            ),
+            tauri::api::Error,
+        > = TauriCommand::new_sidecar("gource")
+            .expect("failed to create `my-sidecar` binary command")
+            .args(&args)
+            .spawn();
 
+        println!("{:?}", res);
+        let command_child = res.unwrap().1;
+    }
     Ok(())
     // ("".to_string())
 }
