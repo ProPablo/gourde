0a7a7e4b20c0361e9939f13afe0df4769a109a54
Leon Ahuja <leon.ahuja@tandm.ai>
2023-08-02 17:46:25 +1000

    not working yet because we have yet to trransform to mutex around child

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,39 +2,40 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::{
+    os::windows::prelude::AsHandle,
     process::{Child, Command},
-    sync::{Arc, Mutex}, os::windows::prelude::AsHandle,
+    sync::{Arc, Mutex},
 };
 
 use anyhow::{bail, Context, Result};
 use tauri::api::process::{Command as TauriCommand, CommandChild};
 use tauri::{AppHandle, Config, Manager, Runtime, State};
 
+type SharedChild = Arc<Child>;
+type ChildContainer = Mutex<Option<SharedChild>>;
+
 struct GourceContainer {
-    child: Mutex<Option<Child>>,
+    child: ChildContainer,
 }
 
-fn kill_old_child(maybe_old_child: &mut Option<Child>) -> Result<bool> {
-    match maybe_old_child.take() {
-        Some(mut old_thread) => {
-            println!("Stopping old child");
-            let res = old_thread.kill()?;
-            Ok(true)
-        }
-        None => Ok(false),
+fn kill_old_child(containter: &GourceContainer) -> Result<bool, String> {
+    let mut maybe_child = containter.child.lock();
+    match &mut maybe_child {
+        Ok(maybe_child) => match maybe_child.take() {
+            Some( old_thread) => {
+                println!("Stopping old child");
+                old_thread.kill().map_err(|e| e.to_string()).map(|_| true)
+            }
+            None => Ok(false),
+        },
+        Err(e) => Err("Failed to lock mutex".to_string()),
     }
 }
 
 #[tauri::command]
-async fn kill_child(state: State<'_, GourceContainer>) -> Result<(), String> {
-    let mut maybe_child = state.child.lock();
-    if let Ok(maybe_child) = &mut maybe_child {
-        match kill_old_child(maybe_child) {
-            Ok(_) => return Ok(()),
-            Err(e) => return Err(e.to_string()),
-        }
-    }
-    Err("Failed to lock mutex".to_string())
+fn kill_child(state: State<'_, GourceContainer>) -> Result<bool, String> {
+    // State.Inner gives access to the struct inside the state as a reference in case
+    kill_old_child(&state.inner())
 }
 
 enum FolderResult {
@@ -57,19 +58,12 @@ async fn is_file_git_repo(path: &str) -> FolderResult {
 }
 
 #[tauri::command]
-async fn run_gource<R: Runtime>(
+async fn run_gource<'a, R: Runtime>(
     app: AppHandle<R>,
     args: Vec<String>,
-    state: State<'_, GourceContainer>,
+    state: State<'a, GourceContainer>,
 ) -> Result<(), String> {
-    #[cfg(windows)]
-    {
-        {
-            let mut maybe_child = state.child.lock().unwrap();
-            if let Err(e) = kill_old_child(&mut maybe_child) {
-                return Err(e.to_string());
-            }
-        }
+
         match is_file_git_repo(&args[0]).await {
             FolderResult::GitRepo => {}
             FolderResult::NotGitRepo => {
@@ -77,6 +71,13 @@ async fn run_gource<R: Runtime>(
             }
             FolderResult::NotFolder => {
                 return Err("Not a folder".to_string());
+            }
+        }
+
+        {
+            let  inner = state.inner();
+            if let Err(e) = kill_old_child(inner) {
+                return Err(e.to_string());
             }
         }
 
@@ -101,22 +102,21 @@ async fn run_gource<R: Runtime>(
         match maybe_output {
             Ok(mut output) => {
                 // TODO: make this into an arc that is shared across a thread that monitors when its dead, also works for checking when window is dead, and subbing to on_window_event
+                let shared_child = Arc::new(Mutex::new(output));
 
-                // let t = std::thread::spawn(move || {
-                //     let _ = output.wait();
-                //     println!("Gource finished");
-                // });
-                let mut maybe_child = state.child.lock().unwrap();
-                *maybe_child = Some(output);
+                let cloned_child = shared_child.clone();
+
+                let t = std::thread::spawn(move || {
+                    let _ = cloned_child.lock().unwrap().wait();
+                    println!("Gource finished");
+                });
+
+                // let mut maybe_child = state.child.lock().unwrap();
+                // *maybe_child = Some(shared_child);
             }
             Err(e) => {
                 return Err(e.to_string());
             }
-        }
-    }
-    #[cfg(not(windows))]
-    {
-        run_gource_not_win(args, &mut maybe_child);
     }
     Ok(())
 }
